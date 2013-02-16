555f877eb4e75e3822b08333c24ef8aaa97efb78
Aaron <l3370x@gmail.com>
2013-02-15 17:58:04 -0700

    slowly starting to add activity and start page.

--- src/com/example/lxgame/MainActivity.java
+++ src/com/example/lxgame/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lxgame;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void startGame(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
 	}
 
 }
