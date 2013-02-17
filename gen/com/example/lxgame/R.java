6d04e4fd46d7d22124d1ef165bb3a5ef54adc8cb
Aaron <l3370x@gmail.com>
2013-02-17 09:35:11 -0700

    starting day 2

--- gen/com/example/lxgame/R.java
+++ gen/com/example/lxgame/R.java
@@ -35,12 +35,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int button1=0x7f090004;
-        public static final int dummy_button=0x7f090002;
+        public static final int action_settings=0x7f090008;
+        public static final int button1=0x7f090006;
+        public static final int dummy_button=0x7f090005;
+        public static final int editText1=0x7f090003;
         public static final int fullscreen_content=0x7f090000;
-        public static final int fullscreen_content_controls=0x7f090001;
-        public static final int textView1=0x7f090003;
+        public static final int fullscreen_content_controls=0x7f090004;
+        public static final int seekBar1=0x7f090001;
+        public static final int seekBar2=0x7f090002;
+        public static final int textView1=0x7f090007;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -51,6 +54,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
+        public static final int add_button=0x7f060007;
         public static final int app_name=0x7f060000;
         public static final int dummy_button=0x7f060005;
         public static final int dummy_content=0x7f060006;
